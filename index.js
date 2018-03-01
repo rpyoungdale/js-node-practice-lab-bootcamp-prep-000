db94d17fd336ebfc309ae5fc4fea98df6132380f
Ryan Youngdale <youngdale95@gmail.com>
2018-03-01 20:33:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // write your code below!
 
-var name = "Ryan"
-var height = "70"
+var name = "Ryan";
+var height = 70;
 
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(parseInt(height)+1)
+console.log("Name:");
+console.log(name);
+console.log("Height:");
+console.log(height + 1);
